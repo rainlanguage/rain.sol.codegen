6528f9ea752638bb24b57df91b1fe587fc0b6f13
thedavidmeister <thedavidmeister@gmail.com>
2024-06-27 17:06:40 +0400

    refactor constant strings

--- src/lib/LibCodeGen.sol
+++ src/lib/LibCodeGen.sol
@@ -45,18 +45,16 @@ library LibCodeGen {
         view
         returns (string memory)
     {
-        string memory functionPointers = LibHexString.bytesToHex(vm, interpreter.buildOpcodeFunctionPointers());
-        return string.concat(
-            "\n",
-            "/// @dev The function pointers known to the interpreter for dynamic dispatch.\n",
-            "/// By setting these as a constant they can be inlined into the interpreter\n",
-            "/// and loaded at eval time for very low gas (~100) due to the compiler\n",
-            "/// optimising it to a single `codecopy` to build the in memory bytes array.\n",
-            "bytes constant OPCODE_FUNCTION_POINTERS =",
-            bytes(functionPointers).length + 43 > MAX_LINE_LENGTH ? NEWLINE_DUE_TO_MAX_LENGTH : " ",
-            "hex\"",
-            functionPointers,
-            "\";\n"
+        return bytesConstantString(
+            vm,
+            string.concat(
+                "/// @dev The function pointers known to the interpreter for dynamic dispatch.\n",
+                "/// By setting these as a constant they can be inlined into the interpreter\n",
+                "/// and loaded at eval time for very low gas (~100) due to the compiler\n",
+                "/// optimising it to a single `codecopy` to build the in memory bytes array.\n"
+            ),
+            "OPCODE_FUNCTION_POINTERS",
+            interpreter.buildOpcodeFunctionPointers()
         );
     }
 
@@ -65,15 +63,17 @@ library LibCodeGen {
         pure
         returns (string memory)
     {
-        return string.concat(
-            "\n",
-            "/// @dev Every two bytes is a function pointer for a literal parser.\n",
-            "/// Literal dispatches are determined by the first byte(s) of the literal\n",
-            "/// rather than a full word lookup, and are done with simple conditional\n",
-            "/// jumps as the possibilities are limited compared to the number of words we\n" "/// have.\n",
-            "bytes constant LITERAL_PARSER_FUNCTION_POINTERS = hex\"",
-            LibHexString.bytesToHex(vm, instance.buildLiteralParserFunctionPointers()),
-            "\";\n"
+        return bytesConstantString(
+            vm,
+            string.concat(
+                "/// @dev Every two bytes is a function pointer for a literal parser.\n",
+                "/// Literal dispatches are determined by the first byte(s) of the literal\n",
+                "/// rather than a full word lookup, and are done with simple conditional\n",
+                "/// jumps as the possibilities are limited compared to the number of words we\n",
+                "/// have.\n"
+            ),
+            "LITERAL_PARSER_FUNCTION_POINTERS",
+            instance.buildLiteralParserFunctionPointers()
         );
     }
 
@@ -82,18 +82,15 @@ library LibCodeGen {
         pure
         returns (string memory)
     {
-        string memory operandHandlerFunctionPointers =
-            LibHexString.bytesToHex(vm, instance.buildOperandHandlerFunctionPointers());
-        return string.concat(
-            "\n",
-            "/// @dev Every two bytes is a function pointer for an operand handler.\n",
-            "/// These positional indexes all map to the same indexes looked up in the parse\n",
-            "/// meta.\n",
-            "bytes constant OPERAND_HANDLER_FUNCTION_POINTERS =",
-            bytes(operandHandlerFunctionPointers).length + 52 > MAX_LINE_LENGTH ? NEWLINE_DUE_TO_MAX_LENGTH : " ",
-            "hex\"",
-            operandHandlerFunctionPointers,
-            "\";\n"
+        return bytesConstantString(
+            vm,
+            string.concat(
+                "/// @dev Every two bytes is a function pointer for an operand handler.\n",
+                "/// These positional indexes all map to the same indexes looked up in the parse\n",
+                "/// meta.\n"
+            ),
+            "OPERAND_HANDLER_FUNCTION_POINTERS",
+            instance.buildOperandHandlerFunctionPointers()
         );
     }
 
@@ -103,35 +100,28 @@ library LibCodeGen {
         returns (string memory)
     {
         AuthoringMetaV2[] memory authoringMeta = abi.decode(authoringMetaBytes, (AuthoringMetaV2[]));
-        string memory parseMeta =
-            LibHexString.bytesToHex(vm, LibGenParseMeta.buildParseMetaV2(authoringMeta, buildDepth));
-        return string.concat(
-            "\n",
-            "/// @dev Encodes the parser meta that is used to lookup word definitions.\n",
-            "/// The structure of the parser meta is:\n",
-            "/// - 1 byte: The depth of the bloom filters\n",
-            "/// - 1 byte: The hashing seed\n",
-            "/// - The bloom filters, each is 32 bytes long, one for each build depth.\n",
-            "/// - All the items for each word, each is 4 bytes long. Each item's first byte\n",
-            "///   is its opcode index, the remaining 3 bytes are the word fingerprint.\n",
-            "/// To do a lookup, the word is hashed with the seed, then the first byte of the\n",
-            "/// hash is compared against the bloom filter. If there is a hit then we count\n",
-            "/// the number of 1 bits in the bloom filter up to this item's 1 bit. We then\n",
-            "/// treat this a the index of the item in the items array. We then compare the\n",
-            "/// word fingerprint against the fingerprint of the item at this index. If the\n",
-            "/// fingerprints equal then we have a match, else we increment the seed and try\n",
-            "/// again with the next bloom filter, offsetting all the indexes by the total\n",
-            "/// bit count of the previous bloom filter. If we reach the end of the bloom\n",
-            "/// filters then we have a miss.\n",
-            "bytes constant PARSE_META =",
-            bytes(parseMeta).length + 34 > MAX_LINE_LENGTH ? NEWLINE_DUE_TO_MAX_LENGTH : " ",
-            "hex\"",
-            parseMeta,
-            "\";\n\n",
-            "/// @dev The build depth of the parser meta.\n",
-            "uint8 constant PARSE_META_BUILD_DEPTH = ",
-            vm.toString(buildDepth),
-            ";\n"
+        return bytesConstantString(
+            vm,
+            string.concat(
+                "/// @dev The parse meta that is used to lookup word definitions.\n",
+                "/// The structure of the parse meta is:\n",
+                "/// - 1 byte: The depth of the bloom filters\n",
+                "/// - 1 byte: The hashing seed\n",
+                "/// - The bloom filters, each is 32 bytes long, one for each build depth.\n",
+                "/// - All the items for each word, each is 4 bytes long. Each item's first byte\n",
+                "///   is its opcode index, the remaining 3 bytes are the word fingerprint.\n",
+                "/// To do a lookup, the word is hashed with the seed, then the first byte of the\n",
+                "/// hash is compared against the bloom filter. If there is a hit then we count\n",
+                "/// the number of 1 bits in the bloom filter up to this item's 1 bit. We then\n",
+                "/// treat this a the index of the item in the items array. We then compare the\n",
+                "/// word fingerprint against the fingerprint of the item at this index. If the\n",
+                "/// fingerprints equal then we have a match, else we increment the seed and try\n",
+                "/// again with the next bloom filter, offsetting all the indexes by the total\n",
+                "/// bit count of the previous bloom filter. If we reach the end of the bloom\n",
+                "/// filters then we have a miss.\n"
+            ),
+            "PARSE_META",
+            LibGenParseMeta.buildParseMetaV2(authoringMeta, buildDepth)
         );
     }
 
@@ -140,19 +130,17 @@ library LibCodeGen {
         pure
         returns (string memory)
     {
-        string memory subParserWordParsers = LibHexString.bytesToHex(vm, subParser.buildSubParserWordParsers());
-        return string.concat(
-            "\n",
-            "/// @dev Real function pointers to the sub parser functions that produce the\n",
-            "/// bytecode that this contract knows about. This is both constructing the subParser\n",
-            "/// bytecode that dials back into this contract at eval time, and mapping\n",
-            "/// to things that happen entirely on the interpreter such as well known\n",
-            "/// constants and references to the context grid.\n",
-            "bytes constant SUB_PARSER_WORD_PARSERS =",
-            bytes(subParserWordParsers).length + 47 > MAX_LINE_LENGTH ? NEWLINE_DUE_TO_MAX_LENGTH : " ",
-            "hex\"",
-            subParserWordParsers,
-            "\";\n"
+        return bytesConstantString(
+            vm,
+            string.concat(
+                "/// @dev The function pointers for the sub parser functions that produce the\n",
+                "/// bytecode that this contract knows about. This is both constructing the subParser\n",
+                "/// bytecode that dials back into this contract at eval time, and mapping\n",
+                "/// to things that happen entirely on the interpreter such as well known\n",
+                "/// constants and references to the context grid.\n"
+            ),
+            "SUB_PARSER_WORD_PARSERS",
+            subParser.buildSubParserWordParsers()
         );
     }
 
@@ -161,15 +149,11 @@ library LibCodeGen {
         view
         returns (string memory)
     {
-        string memory integrityFunctionPointers = LibHexString.bytesToHex(vm, deployer.buildIntegrityFunctionPointers());
-        return string.concat(
-            "\n",
+        return bytesConstantString(
+            vm,
             "/// @dev The function pointers for the integrity check fns.\n",
-            "bytes constant INTEGRITY_FUNCTION_POINTERS =",
-            bytes(integrityFunctionPointers).length + 51 > MAX_LINE_LENGTH ? NEWLINE_DUE_TO_MAX_LENGTH : " ",
-            "hex\"",
-            integrityFunctionPointers,
-            "\";\n"
+            "INTEGRITY_FUNCTION_POINTERS",
+            deployer.buildIntegrityFunctionPointers()
         );
     }
 
@@ -181,6 +165,24 @@ library LibCodeGen {
             "bytes32 constant DESCRIBED_BY_META_HASH = bytes32(",
             vm.toString(keccak256(describedByMeta)),
             ");\n"
+        );
+    }
+
+    function bytesConstantString(Vm vm, string memory comment, string memory name, bytes memory data)
+        internal
+        pure
+        returns (string memory)
+    {
+        string memory hexData = LibHexString.bytesToHex(vm, data);
+        return string.concat(
+            "\n",
+            comment,
+            "bytes constant ",
+            name,
+            bytes(hexData).length + bytes(name).length + 15 + 7 + 2 > MAX_LINE_LENGTH ? NEWLINE_DUE_TO_MAX_LENGTH : " ",
+            " = hex\"",
+            hexData,
+            "\";\n"
         );
     }
 }
